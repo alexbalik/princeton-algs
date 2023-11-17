8f161b33d484ead89544172a8ee43e3f5ad0f8a8
Alex Balik <92473619+alexbalik@users.noreply.github.com>
2023-11-17 17:54:42 -0500

    Convert sudo code into functional java code
    Translated sudo code into something that compiles and runs. Still have
    a problem where the program keeps asking for inputs but never stops,
    even if the user enters an empty string

--- RandomWord.java
+++ RandomWord.java
@@ -4,10 +4,31 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     public static void main(String[] args) {
+        int i = 1; // this is the counter. i=1 labels the first word entered by user
+        float probabilityofChamp = 1; // start at probability = 100%. Might have to change this?
+        String champion = ""; // initialize champion variable
+        String tempWord = ""; // initialize temporary word variable
+        while (StdIn.isEmpty() == false) {
+            tempWord
+                    = StdIn.readString(); // assign the user's inputted work to a temporary variable
+            probabilityofChamp = 1
+                    / i; // calculate the probability of the ith word being the champion
+            if (StdRandom.bernoulli(probabilityofChamp) == true) {
+                champion = tempWord;
+            }
+            else {
+                i = i + 1;
+            }
 
 
+        }
+        StdOut.println(champion);
     }
 }
 
